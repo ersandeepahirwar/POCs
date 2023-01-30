6d1bfede6b4abb22a2fddd931f62ffed052551fd
ersandeepahirwar <codeysandeep@gmail.com>
2023-01-30 16:49:20 +0530

    Implement the Device Orientation Detector and Angular Velocity Detector

--- Gyroscope POCs/Angular Velocity/script.js
+++ Gyroscope POCs/Angular Velocity/script.js
@@ -20,3 +20,33 @@ sensor.onreading = () => {
 sensor.onerror = (event) => {
   alert(event.error.name, event.error.message);
 };
+
+const enableGyroscope = () => {
+  const sensor = new RelativeOrientationSensor();
+  Promise.all([
+    navigator.permissions.query({ name: "accelerometer" }),
+    navigator.permissions.query({ name: "gyroscope" }),
+  ]).then((results) => {
+    if (results.every((result) => result.state === "granted")) {
+      sensor.start();
+
+      sensor.onreading = () => {
+        x = sensor.x * 100;
+        y = sensor.y * 100;
+        z = sensor.z * -33;
+
+        const wheel = document.querySelector(".wheel");
+
+        wheel.style.left = wheel.offsetLeft + x + "px";
+        wheel.style.top = wheel.offsetTop - y + "px";
+        wheel.style.transform = `rotate(${z}deg)`;
+      };
+
+      sensor.onerror = (event) => {
+        alert(event.error.name, event.error.message);
+      };
+    } else {
+      alert("No Permissions to use Relative Orientation Sensor");
+    }
+  });
+};
