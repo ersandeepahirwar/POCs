60d740dacec7e7a8301925d4786105eced2cc470
ersandeepahirwar <codeysandeep@gmail.com>
2023-01-30 13:57:22 +0530

    Implement the Device Orientation Detector and Angular Velocity Detector

--- Gyroscope POCs/Angular Velocity/script.js
+++ Gyroscope POCs/Angular Velocity/script.js
@@ -8,8 +8,10 @@ let z;
 sensor.start();
 sensor.onreading = () => {
   x = sensor.x * 100;
-  y = sensor.y;
-  z = sensor.z;
+  y = sensor.y * 100;
+  z = sensor.z * 100;
 
   wheel.style.left = wheel.offsetLeft + x + "px";
+  wheel.style.top = wheel.offsetTop - y + "px";
+  wheel.style.transform = `rotate(${x}deg)`;
 };
